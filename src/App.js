6b6efc682434ddf64c9570e86a3ebad023b0d521
Anshuman1609 <anshumandubey1609@gmail.com>
2024-04-10 17:06:27 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import {
 } from "react-router-dom";
 import Home from "./components/home/Home";
 import NotFound from "./components/NotFound";
+import List from "./components/list/List";
+import Hotel from "./components/hotel/Hotel";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element = {<Home/>} />
+        <Route exact path="/hotels" element = {<List/>} />
+        <Route exact path="/hotels/:id" element = {<Hotel/>} />
+
         <Route exact path="*" element = {<NotFound/>} />
       </Routes>
     </Router>
