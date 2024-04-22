b4f1aedffa05a94f556f3a7acca21f2db1965706
Anshuman1609 <anshumandubey1609@gmail.com>
2024-04-22 23:28:51 +0530

    hotel page

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import "./hotel.css";
+import Navbar from "../../components/navbar/Navbar.jsx";
+import Header from "../../components/header/Header.jsx";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Hotel = () => {
   return (
-    <div>Hotel</div>
+    <>
+      <Navbar />
+      <Header type="list" />
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <h1 className="hotalTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot}/>
+            <span>Elton St125 New York</span>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
